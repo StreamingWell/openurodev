20d32489eb4d10783e853091a7901f8bb8fd5805
brandenmittra <branden@univrs.co.uk>
2015-07-12 16:09:39 +0100

    edit for testing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
   # remove this before deploying to production
-  # before_filter :authenticate
+   before_filter :authenticate
 
   def home
   end
@@ -24,7 +24,7 @@ class PagesController < ApplicationController
   def zinc
   end
 
-  def pi
+  def test
   end
 
   def preregistration
@@ -36,10 +36,10 @@ class PagesController < ApplicationController
 
   #protected
 
-  #  def authenticate
-  #	  authenticate_or_request_with_http_basic do |username, password|
-  #	  username == "admin" && password == "urology2014"
-  #	 end
-   # end
+    def authenticate
+  	  authenticate_or_request_with_http_basic do |username, password|
+  	  username == "admin" && password == "inhalereducation"
+  	 end
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Openmicrosite::Application.routes.draw do
 
   # Home
-  root :to => 'pages#preregistration'
+  root :to => 'pages#test'
 
   # Live Webinar
   #root :to => 'pages#live'
@@ -11,15 +11,15 @@ Openmicrosite::Application.routes.draw do
   devise_for :users
 
   # Static pages
-  get 'home' => 'pages#archive'
-  get 'live' => 'pages#live'
-  get 'archive' => 'pages#archive'
-  get 'zinc' => 'pages#zinc'
-  get 'holding' => 'pages#holding'
+  #get 'home' => 'pages#archive'
+  #get 'live' => 'pages#live'
+  #get 'archive' => 'pages#archive'
+  #get 'zinc' => 'pages#zinc'
+  #get 'holding' => 'pages#holding'
   get 'login' => 'pages#login'
-  get 'evaluation' => 'pages#evaluation'
+  #get 'evaluation' => 'pages#evaluation'
   get 'help' => 'pages#help'
-  # get 'pi' => 'pages#pi'
+  get 'test' => 'pages#test'
 
   # Feedback/submissions
   match 'contact' => 'contact#new', :as => 'contact', :via => :get
